b22b6c80497544aeea8830e8ad06601580206242
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2022-06-22 05:07:32 +0600

    Update credits.js

--- src/Interactions/ApplicationCommand/ChatInput/credits.js
+++ src/Interactions/ApplicationCommand/ChatInput/credits.js
@@ -14,12 +14,12 @@ module.exports = {
         {
           name: 'Bot Avatar',
           value:
-            'Logo by @555081092177330187' +
+            'Logo by <@555081092177330187>' +
               '\nBase Image: Unciv icon v5' +
                 ' • [Source](https://github.com/yairm210/Unciv/blob/345114ed935c72f3e1548b4497c7f42bf9f1258d/extraImages/Unciv%20icon%20v5.png)' +
                 ' • [LICENSE](https://github.com/yairm210/Unciv/blob/master/LICENSE)' +
               '\nMultiplayer by Roy Charles from NounProject.com • [Source](https://thenounproject.com/icon/multiplayer-1215652/)',
-        }
+        },
       ],
     }).toResponse();
   },
