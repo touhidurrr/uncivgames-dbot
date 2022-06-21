8307e1ee989416321d4764c8be8bd745052cde09
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2022-06-22 05:05:52 +0600

    Update credits.js

--- src/Interactions/ApplicationCommand/ChatInput/credits.js
+++ src/Interactions/ApplicationCommand/ChatInput/credits.js
@@ -19,7 +19,7 @@ module.exports = {
                 ' • [Source](https://github.com/yairm210/Unciv/blob/345114ed935c72f3e1548b4497c7f42bf9f1258d/extraImages/Unciv%20icon%20v5.png)' +
                 ' • [LICENSE](https://github.com/yairm210/Unciv/blob/master/LICENSE)' +
               '\nMultiplayer by Roy Charles from NounProject.com • [Source](https://thenounproject.com/icon/multiplayer-1215652/)',
-      
+        }
       ],
     }).toResponse();
   },
