e89fe0149da15a4b9bc4a66bf087b76499a6ee7c
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2023-05-24 19:36:33 +0600

    Update scheduled.js

--- src/scheduled.js
+++ src/scheduled.js
@@ -36,7 +36,7 @@ async function sendUncivUpdateNotification() {
     Discord(
       'POST',
       `/channels/${Channels.uncivUpdates}/messages`,
-      new Message({
+      new Message('<@1110904546642886679>').addEmbed({
         fields: [
           {
             name: 'Release Id',
