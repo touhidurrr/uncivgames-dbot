320e5ed55df50a1cc54235ad920a757689a4bc92
Md. Touhidur Rahman <46617994+touhidurrr@users.noreply.github.com>
2024-05-14 18:24:26 +0600

    fix credits.js

--- src/Interactions/ApplicationCommand/ChatInput/credits.js
+++ src/Interactions/ApplicationCommand/ChatInput/credits.js
@@ -10,12 +10,12 @@ module.exports = {
         {
           name: 'Author',
           value:
-            '@touhidurrr • [Github](https://github.com/touhidurrr) • [Email](https://tinyurl.com/dbotmail)',
+            '<@577491473100963840> • [Github](https://github.com/touhidurrr) • [Email](https://tinyurl.com/dbotmail)',
         },
         {
           name: 'Bot Avatar',
           value:
-            'Logo by @caballeroarepa' +
+            'Logo by <@555081092177330187>' +
             '\nBase Image: Unciv icon v5' +
             ' • [Source](https://github.com/yairm210/Unciv/blob/345114ed935c72f3e1548b4497c7f42bf9f1258d/extraImages/Unciv%20icon%20v5.png)' +
             ' • [LICENSE](https://github.com/yairm210/Unciv/blob/master/LICENSE)' +
