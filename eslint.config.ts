489c18571ae70e9a9dbee6a731f2f7e1094f2f77
Md. Touhidur Rahman <touhidurrr@pm.me>
2026-02-22 04:58:33 +0600

    update dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,15 @@
-import eslint from '@eslint/js';
+import js from '@eslint/js';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  { ignores: ['prisma/generated'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  globalIgnores(['prisma/generated']),
 ]);
