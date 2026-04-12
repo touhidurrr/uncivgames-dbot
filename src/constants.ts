073a4c01df78a86949e454bf85752f06a1e8f4db
Md. Touhidur Rahman <touhidurrr@pm.me>
2026-04-13 05:59:30 +0600

    fix broken support embed title

--- src/constants.ts
+++ src/constants.ts
@@ -38,7 +38,7 @@ export const SUPPORT_EMBED_MESSAGE = `Enjoying **UncivServer.xyz**? **[${SUPPORT
 
 export const SUPPORT_EMBED: Readonly<APIEmbed> = {
   author: {
-    name: 'Support [UncivServer.xyz](https://uncivserver.xyz) & <@866759632617996308>!',
+    name: 'Support UncivServer.xyz & UncivGames Democracy Bot!',
     icon_url: 'https://media.giphy.com/media/TDQOtnWgsBx99cNoyH/giphy.gif',
   },
   description: SUPPORT_EMBED_MESSAGE,
